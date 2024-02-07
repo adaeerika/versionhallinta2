cecfaff1927217b64a6e3e80f57ec6819ce71c40
adaeerika <ada23003@student.hamk.fi>
2024-02-07 14:38:50 +0200

    Ensimmäinen commit

---
+++ src/OhjelmointiHarjoitus.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OhjelmointiHarjoitus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+        String answer = "Emma";
+        int guesses = 0;
+        
+        System.out.println("Guess my name (type stop to exit)");
+        
+    }
+}
