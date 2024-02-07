c8067bb00f170ef91e7c7227c499bd33809f54d5
adaeerika <ada23003@student.hamk.fi>
2024-02-07 14:41:49 +0200

    Tein koodin loppuun

--- src/OhjelmointiHarjoitus.java
+++ src/OhjelmointiHarjoitus.java
@@ -10,7 +10,7 @@ public class OhjelmointiHarjoitus {
         int guesses = 0;
         
         System.out.println("Guess my name (type stop to exit)");
-        
+
         while (true) 
 	        {
 	            String guess = scanner.nextLine();
@@ -20,7 +20,27 @@ public class OhjelmointiHarjoitus {
 	            	guesses++;
 	                System.out.println("Congratulations!");
 	                break; 
+	            } 
+	            
+	            else if (guess.equalsIgnoreCase("stop")) 
+	            {
+	                break; 
 	            }
-    }
-}
+	            
+	            else 
+	            {
+	            	guesses++;
+	            	System.out.println("Guess my name (type stop to exit)"); 
+	            }
+	        }
+        
+        if (guesses > 0 && !answer.equalsIgnoreCase("stop"))
+        {
+        System.out.println("You guessed " + (guesses) + " times.");
+        }
+        
+        scanner.close();
+		
+	}
+
 }
